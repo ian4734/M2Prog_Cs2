a27e2c6fb61e41492387021ab87dfd1150942cc4
Ian <40573@ma-web.nl>
2025-11-21 14:53:19 +0100

    4rd commit

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -8,6 +8,49 @@ internal class Program
         program.Run();  // de variable is een void. het is een empty type variable
     }
     internal void Run()
-    {    
+    {
+        Console.WriteLine("Hoe laat is het");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine(antwoord);
+    }
+
+    internal void Vraag1()
+    {
+        Console.WriteLine("how long do you think you would survive in a zombie apocalypse?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine(antwoord);
+
+    }
+    internal void Vraag2()
+    {
+        Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine(antwoord);
+
+    }
+     internal void Vraag3()
+    {
+        Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine(antwoord);
+
+    
+    } internal void Vraag4()
+    {
+        Console.WriteLine("Why do round pizzas come in square boxes?");
+        string antwoord = Console.ReadLine();
+
+        Console.WriteLine(antwoord);
+
+    }
+    internal void Vraag5()
+    {
+        Console.WriteLine("Has someone caught you dancing in front of the mirror?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
     }
 }
