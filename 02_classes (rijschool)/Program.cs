7f1e526b2141762b72e89cc565ac1c086a8897e6
Ian <40573@ma-web.nl>
2025-11-28 14:33:09 +0100

    second commit

--- 02_classes (rijschool)/Program.cs
+++ 02_classes (rijschool)/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _02_classes;
+﻿using System.Data;
+
+namespace _02_classes;
 
 class Program
 {
@@ -9,40 +11,60 @@ class Program
 }
 internal class auto
 {
-     
+    internal bool automaat;
+    internal string merk;
+    internal string kenteken;
+    internal int Kilometerstand;
 }
 
 internal class RijLeraar
 {
-     
+    internal string naam;
+    internal string leeftijd;
+    internal bool zzp;
 }
 
 internal class  Student
 {
-     
+    internal string naam;
+    internal int leeftijd;
+    internal LesPakket lesPakket;
+    internal RijLeraar rijLeraar;
+    internal TheorieTest theorieTest;
+    internal RijTest rijTest;
 }
 
 internal class LesPakket
 {
-     
+    internal int UrenGekocht;
+    internal int UrenVerbruikt;
+    internal int examenPogingen;
+    internal bool automaat;
 }
 
 internal class RijTest
 {
-
+    internal bool gehaald;
+    internal DateTime afnameDatum;
 }
 
 internal class TheorieTest
 {
-     
+    internal int aantelFouten;
+    internal bool gehaald;
+     internal DataTime afnameDatum;
 }
 
 internal class Dag
 {
-     
+    internal DateTime datum;
+     internal lesUur[] lesUren;
 }
 
 internal class lesUur
 {
-     
+    internal int tijd;
+    internal Student student;
+    internal RijLeraar rijLeraar;
+    internal auto auto;
 }
