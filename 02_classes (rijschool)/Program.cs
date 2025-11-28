c1259a1e45e98cc5b08b091967fe2687e208fb0a
Ian <40573@ma-web.nl>
2025-11-28 14:20:32 +0100

    les2 first commit

---
+++ 02_classes (rijschool)/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _02_classes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+internal class auto
+{
+     
+}
+
+internal class RijLeraar
+{
+     
+}
+
+internal class  Student
+{
+     
+}
+
+internal class LesPakket
+{
+     
+}
+
+internal class RijTest
+{
+
+}
+
+internal class TheorieTest
+{
+     
+}
+
+internal class Dag
+{
+     
+}
+
+internal class lesUur
+{
+     
+}
