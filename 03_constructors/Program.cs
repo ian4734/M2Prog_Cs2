b829e8cbf16d9ab187d5efa01cb6c605957edefd
Ian <40573@ma-web.nl>
2025-12-05 14:42:21 +0100

    first commit les3

---
+++ 03_constructors/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03_constructors;
+
+class Program;
+
+
+private void run()
+{
+    QuizVraag quizVraag = new QuizVraag("hier is de vraag", "hier is het antwoord");
+}
+
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace M2Prog_Cs2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
