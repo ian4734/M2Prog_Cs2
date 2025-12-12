53dee2def4183045fb6da21965a572a4b66cdef7
Ian <40573@ma-web.nl>
2025-12-12 15:05:27 +0100

    first commit week 4

---
+++ 04_lists/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace _04_lists;
+
+class Program
+{
+    string[] characters = new string[]
+     {
+         "pac - man",
+
+          "samus",
+
+          "Crash Bandicoot",
+
+         "Spyro the Dragon",
+
+          "DonkeyKong",
+
+          "Mario",
+
+          "Luigi",
+
+          "Astro",
+
+          "Solid Snake",
+
+         "Kratos",
+
+          "Sonic the Hedgehog",
+
+          "Link",
+
+     };
+
+
+    List<string> characterlist = new List<string>();
+    
+
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.run();
+
+    }
+    internal void run()
+    {
+        characterlist.Add("odin");
+        foreach (string item in characters)
+        {
+            characterlist.Add(item);
+        }
+       foreach (string item in characters)
+       {
+           Console.WriteLine(item);
+       }
+    }
+}
